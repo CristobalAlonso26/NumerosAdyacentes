0ecda747a4188c7278669b72fcd11f938b7acc7a
cristobal <c.ramos07@ufromail.cl>
2025-05-08 13:56:26 -0400

    Test

---
+++ src/test/java/NumerosAdyacentesTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumerosAdyacentesTest {
+    @Test
+    public void funcionamiento(){
+        int array[]={1, -4, 2, 2, 5, -1};
+        assertEquals(10,NumerosAdyacentes.productoAdyacentes(array));
+    }
+    @Test
+    public void Intercalados(){
+        int array[]={1,-2,3,-4,5,-6};
+        assertEquals(-2,NumerosAdyacentes.productoAdyacentes(array));
+    }
+    @Test
+    public void SoloNegativos(){
+        int array[]={-1,-2,-3,-4,-5,-6,-7,-8,-9,-10};
+        assertEquals(90,NumerosAdyacentes.productoAdyacentes(array));
+    }
+    @Test
+    public void PrimeraSeaMayor(){
+        int array[]={1000,999,3,4,-6,5};
+        assertEquals(999000,NumerosAdyacentes.productoAdyacentes(array));
+    }
+    @Test
+    public void SoloCeros(){
+        int array[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        assertEquals(0,NumerosAdyacentes.productoAdyacentes(array));
+    }
+}
