e850c32d0ac87d82a126ad445fe20be2ed9d3985
cristobal <c.ramos07@ufromail.cl>
2025-05-08 18:37:27 -0400

    añadí metodo validarArray y lo agregue en metodo productosAdyacentes

--- src/main/java/NumerosAdyacentes.java
+++ src/main/java/NumerosAdyacentes.java
@@ -1,5 +1,6 @@
 public class NumerosAdyacentes {
     public static int productoAdyacentes(int[] array){
+        validarArray(array);
         int multiplicacionMayor = array[0]*array[1];
         for (int i=1;i<array.length-1;i++){
             if(array[i]*array[i+1]>multiplicacionMayor){
@@ -8,4 +9,11 @@ public class NumerosAdyacentes {
         }
         return multiplicacionMayor;
     }
+    public static boolean validarArray(int[] array){
+        if (array.length < 2){
+            throw new IllegalArgumentException("el array debe tener 2 elementos como minimo");
+        }
+        return true;
+    }
+
 }
